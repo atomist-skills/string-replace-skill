45737ebf7a296716beae6f94390278ae6a18d16e
Jim Clark <slimslenderslacks@gmail.com>
2020-04-30 22:51:05 -0700

    Bump string-replace-skill to 0.2.49 and spit metadata

--- src/atomist/skill_meta.cljs
+++ src/atomist/skill_meta.cljs
@@ -3,6 +3,6 @@
 
 (def module-name "string-replace-skill")
 (def path ".")
-(def version "0.2.48")
-(def tag "v0.2.48")
-(def generated-at "2020-05-01T04:21:37Z")
+(def version "0.2.49")
+(def tag "v0.2.49")
+(def generated-at "2020-05-01T05:51:05Z")
